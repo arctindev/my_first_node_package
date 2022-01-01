1e505c5e80ffa2c6e0131f36f16d7828d65e1b00
arctindev <arctindev@gmail.com>
2022-01-01 20:05:15 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+export const printMsg = function() {
+    console.log("This is a message from the demo package");
+}
+
+export const printMsg2 = function() {
+    console.log("This is a 2 message from the demo package");
+}
+
+
+export default {
+    printMsg,
+    printMsg2
+};
